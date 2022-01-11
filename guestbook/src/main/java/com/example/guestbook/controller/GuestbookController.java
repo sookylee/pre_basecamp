59f1cba8d190990289aade823c6e52eee7040d74
sookylee <nhn@AL01567979.local>
2022-01-11 15:00:28 +0900

    #2 wrote register controller

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,13 +1,16 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
 import lombok.extern.log4j.Log4j2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -29,5 +32,23 @@ public class GuestbookController {
 
         model.addAttribute("result", service.getList(pageRequestDTO));
 
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto..." + dto);
+
+        //number of new entity
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
