4200323083416db5c1f5fd6e4f73a9f98b612afa
sookylee <nhn@AL01567979.local>
2022-01-11 16:58:35 +0900

    #3 dto 수정, 검색 처리 service 추가

--- guestbook/src/main/java/com/example/guestbook/dto/PageRequestDTO.java
+++ guestbook/src/main/java/com/example/guestbook/dto/PageRequestDTO.java
@@ -13,6 +13,9 @@ import org.springframework.data.domain.Sort;
 public class PageRequestDTO {
     private int page;
     private int size;
+    //for searching
+    private String type;
+    private String keyword;
 
     public PageRequestDTO(){
         this.page = 1;
--- guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.service;
 
+import com.fasterxml.jackson.databind.util.ArrayBuilders;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -7,10 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.example.guestbook.dto.GuestbookDTO;
-import com.example.guestbook.entity.Guestbook;
-import com.example.guestbook.repository.GuestbookRepository;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.dto.PageResultDTO;
+import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.example.guestbook.repository.GuestbookRepository;
 
 import java.util.function.Function;
 import java.util.Optional;
@@ -42,7 +46,9 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = repository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO); //검색 조건 처리
+
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable);
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
@@ -77,5 +83,41 @@ public class GuestbookServiceImpl implements GuestbookService {
 
             repository.save(entity);
         }
+    }
+
+
+    //querydsl 처리
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO){
+
+        String type = requestDTO.getType();
+        String keyword = requestDTO.getKeyword();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L); //gno > 0
+
+        booleanBuilder.and(expression);
+
+        if(type == null || type.trim().length() == 0){ //검색 조건이 없는 경우
+            return booleanBuilder;
+        }
+
+        //검색 조건별
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if(type.contains("t")){
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if(type.contains("c")){
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if(type.contains("w")){
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
     }
 }
