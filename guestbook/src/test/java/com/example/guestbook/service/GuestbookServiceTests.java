124a5a51e1acc8bec817b7265978d4307280ab18
sookylee <nhn@AL01567979.local>
2022-01-11 14:12:29 +0900

    #1 pagination

--- guestbook/src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.guestbook.service;
 
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,4 +26,25 @@ public class GuestbookServiceTests {
         System.out.println(service.register(guestbookDTO));
 
     }
+
+    @Test
+    public void testList(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("---------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("---------------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
 }
