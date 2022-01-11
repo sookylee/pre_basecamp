9aeda81f13dcc52fcfef48faa81e0e77a201161f
sookylee <nhn@AL01567979.local>
2022-01-11 13:32:04 +0900

    #1 dto 처리

--- guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.guestbook.service;
 
-import com.example.guestbook.entity.Guestbook;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto){
@@ -19,7 +24,8 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         log.info(entity);
 
-        return null;
+        repository.save(entity);
 
+        return entity.getGno();
     }
 }
---
+++ guestbook/src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,27 @@
+package com.example.guestbook.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.entity.Guestbook;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister(){
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(service.register(guestbookDTO));
+
+    }
+}
