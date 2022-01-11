2fc7f78736ea5e7af9512acaea5a5c9371e959c3
sookylee <nhn@AL01567979.local>
2022-01-11 09:32:31 +0900

    #1 rendering html

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -14,6 +15,6 @@ public class GuestbookController {
     public String list(){
         log.info("list...........");
 
-        return "/guestbook/list";
+        return "guestbook/list";
     }
 }
