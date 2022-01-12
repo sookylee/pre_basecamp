334286db6c62ae6248a635baa3a3c99c38f25419
sookylee <nhn@AL01567979.local>
2022-01-12 17:24:31 +0900

    #8 특정 영화의 모든 리뷰와 회원의 닉네임

--- mreview/src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ mreview/src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -1,7 +1,14 @@
 package org.zerock.mreview.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.mreview.entity.Review;
+import org.zerock.mreview.entity.Movie;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }
