79c3689d0242f678c1de8a033b6ce680fbccd148
sookylee <nhn@AL01567979.local>
2022-01-13 16:44:56 +0900

    #17 movie controller

--- mreview/src/main/java/org/zerock/mreview/Controller/MovieController.java
+++ mreview/src/main/java/org/zerock/mreview/Controller/MovieController.java
@@ -1,24 +1,38 @@
-package org.zerock.mreview.Controller;
+package org.zerock.mreview.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.service.MovieService;
 
 @Controller
 @RequestMapping("/movie")
 @Log4j2
+@RequiredArgsConstructor
 public class MovieController {
 
-    @GetMapping("/register")
-    public void register(){
+    private final MovieService movieService;
 
+    @GetMapping("/register")
+    public void register() {
+
+    }
+
+    @PostMapping("/register")
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
+        log.info("movieDTO: " + movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
     }
 }
