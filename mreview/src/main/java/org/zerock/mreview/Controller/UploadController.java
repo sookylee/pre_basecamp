e9e96bf5c0df5f470f084a91ccfdfc32605b1be0
sookylee <nhn@AL01567979.local>
2022-01-13 10:13:49 +0900

    #13 이미지 업로드 처리

--- mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
@@ -2,8 +2,11 @@ package org.zerock.mreview.Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +14,9 @@ import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -39,9 +45,14 @@ public class UploadController {
                 return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
-            //실제 파일 이름 ie나 edge는 전체 경로가 들어오므로
+
             String originalName = uploadFile.getOriginalFilename();
-            String fileName = originalName.substring(originalName.lastIndexOf("\\" + 1));
+            String fileName = originalName;
+
+            //실제 파일 이름 ie나 edge는 전체 경로가 들어오므로
+            if(originalName.lastIndexOf("\\") != -1){
+                fileName = originalName.substring(originalName.lastIndexOf("\\" + 1));
+            }
 
             log.info("fileName: " + fileName);
 
@@ -55,6 +66,7 @@ public class UploadController {
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
 
             Path savePath = Paths.get(saveName);
+            //System.out.println(saveName);
 
             try{
                 uploadFile.transferTo(savePath);
@@ -65,6 +77,36 @@ public class UploadController {
 
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+
+        ResponseEntity<byte[]> result = null;
+
+        try{
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            //MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            //파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e){
+            log.error(e.getMessage());
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
 
     private String makeFolder() {
 
