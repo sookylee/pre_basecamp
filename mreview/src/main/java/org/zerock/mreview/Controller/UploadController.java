67eeac619aa6fba3621125d58b30a059032d2e18
sookylee <nhn@AL01567979.local>
2022-01-13 12:10:11 +0900

    fixed #14 썸네일이미지로 변경

--- mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.mreview.dto.UploadResultDTO;
 
+import net.coobird.thumbnailator.Thumbnailator;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
+++ mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -18,6 +18,18 @@ public class UploadResultDTO implements Serializable {
     public String getImageURL(){
         try {
             return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public String getThumbnailURL(){
+        try {
+            return URLEncoder.encode(folderPath + "/" + "s_" + uuid + "_" + fileName, "UTF-8");
+
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
