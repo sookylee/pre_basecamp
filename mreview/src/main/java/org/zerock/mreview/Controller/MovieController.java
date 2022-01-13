ecfc63bb38f20fcfea5b0b45c61a3bb6983f17eb
sookylee <nhn@AL01567979.local>
2022-01-13 16:17:29 +0900

    #17 영화 등록 화면

---
+++ mreview/src/main/java/org/zerock/mreview/Controller/MovieController.java
@@ -0,0 +1,24 @@
+package org.zerock.mreview.Controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/movie")
+@Log4j2
+public class MovieController {
+
+    @GetMapping("/register")
+    public void register(){
+
+    }
+}
