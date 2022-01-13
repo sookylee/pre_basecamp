f8074104493db73f74e43c4843e2df8976045869
sookylee <nhn@AL01567979.local>
2022-01-13 09:34:27 +0900

    #13 이미지 업로드 결과 반환

--- mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/Controller/UploadController.java
@@ -2,9 +2,12 @@ package org.zerock.mreview.Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,14 +27,16 @@ public class UploadController {
     private String uploadPath;
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile: uploadFiles){
 
             //이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false){
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             //실제 파일 이름 ie나 edge는 전체 경로가 들어오므로
@@ -55,6 +62,8 @@ public class UploadController {
                 e.printStackTrace();
             }
         }
+
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
